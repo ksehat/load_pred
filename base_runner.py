70016cb8826b43f980de580c6c77b7cef1947cbc
ksehat <kan.sehat@gmail.com>
2023-04-30 08:53:09 +0330

    api adresses are changed

--- base_runner.py
+++ base_runner.py
@@ -8,12 +8,12 @@ from datetime import datetime as dt
 while True:
     if dt.now().hour == 12 and dt.now().minute == 1:
         token = api_token_handler()
-        r = requests.get(url='http://192.168.115.10:8083/api/Robots/GetAllFutureFlights',
+        r = requests.get(url='http://192.168.115.10:8081/api/Robots/GetAllFutureFlights',
                          headers={'Authorization': f'Bearer {token}'})
         if json.loads(r.content)['msg']:
             unsuccessful_pk = [int(x) for x in json.loads(r.text)['msg'].split(',')[1:]]
             for one_pk in unsuccessful_pk:
-                r2 = requests.post(url='http://192.168.115.10:8083/api/Robots/GetAllFlightsWithSameCondition',
+                r2 = requests.post(url='http://192.168.115.10:8081/api/Robots/GetAllFlightsWithSameCondition',
                                    json={'pkFlightInformation': one_pk},
                                    headers={'Authorization': f'Bearer {token}',
                                             'Content-type': 'application/json',
