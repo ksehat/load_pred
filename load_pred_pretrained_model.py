b551a7e8e106aed731d6ef3484faab176b228ebf
ksehat <kan.sehat@gmail.com>
2023-05-24 09:35:39 +0330

    load prediction is refactored in order to get the past data from the API.

--- load_pred_pretrained_model.py
+++ load_pred_pretrained_model.py
@@ -6,51 +6,62 @@ import pickle
 import numpy as np
 from tensorflow import keras
 from functions import api_token_handler
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_pred_pretrained_model(df_dict):
-    df = pd.DataFrame(df_dict)
-    df.rename(columns={'Is_holiday': 'is_holiday', 'Route': 'FlightRoute', 'GregorianDate': 'Departure'}, inplace=True)
-    df['Departure'] = pd.to_datetime(df['Departure'])
-    df['FlightRoute'] = df['FlightRoute'].str.replace('>', '-')
-    df['PaxWeight'] = 0
+    df_future = pd.DataFrame(df_dict)
+    df_future.rename(columns={'Is_holiday': 'is_holiday', 'Route': 'route', 'Departure': 'departure', 'PkFlightInformation': 'pkFlightInformation'}, inplace=True)
+    df_future['departure'] = pd.to_datetime(df_future['departure'])
+    df_future['paxWeight'] = 0
 
-    df_old = pd.read_excel('data/df.xlsx')
+    token = api_token_handler()
+
+    df_past = pd.DataFrame(json.loads(requests.get(url='http://192.168.115.10:8081/api/Robots/GetAllPastFlights',
+                           headers={'Authorization': f'Bearer {token}',
+                                    'Content-type': 'application/json',
+                                    }
+                           ).text)['getAllPastFlightsResponseItemViewModels'])
+
     with open('label_encoder.pkl', 'rb') as f:
         le = pickle.load(f)
 
-    for i in range(len(df)):
+    df_future['route'] = df_future['route'].apply(lambda x: x.replace(">", "-"))
+    df_past['route'] = df_past['route'].apply(lambda x: x.replace(">", "-"))
+
+
+    for i in range(len(df_future)):
         try:
-            pkFlightInformation = df.iloc[i:i + 1, :]['PkFlightInformation'].values[0]
-            flightdate = df.iloc[i:i + 1, :]['Departure'].values[0]
-            df0 = pd.DataFrame()
-            df0 = pd.concat([df_old, df.iloc[i:i + 1, :]], axis=0, ignore_index=True)
-            df0.drop(['GregorianDate', 'PkFlightInformation'], axis=1, inplace=True)
+            pkFlightInformation = df_future.iloc[i:i + 1, :]['pkFlightInformation'].values[0]
+            flightdate = df_future.iloc[i:i + 1, :]['departure'].values[0]
 
-            df0['FlightRoute'] = le.transform(df0['FlightRoute'])
+            df0 = pd.concat([df_past, df_future.iloc[i:i + 1, :]], axis=0, ignore_index=True)
+            df0.drop(['pkFlightInformation'], axis=1, inplace=True)
 
-            df0['year'] = np.array(pd.DatetimeIndex(df0['Departure']).year)
-            df0['month'] = np.array(pd.DatetimeIndex(df0['Departure']).month)
-            df0['day'] = np.array(pd.DatetimeIndex(df0['Departure']).day)
-            df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['Departure']).dayofweek)
-            df0['hour'] = np.array(pd.DatetimeIndex(df0['Departure']).hour)
+            df0['route'] = df0['route'].apply(lambda x: le.transform([x])[0] if x in le.classes_ else -1)
+
+            df0['year'] = np.array(pd.DatetimeIndex(df0['departure']).year)
+            df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
+            df0['day'] = np.array(pd.DatetimeIndex(df0['departure']).day)
+            df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['departure']).dayofweek)
+            df0['hour'] = np.array(pd.DatetimeIndex(df0['departure']).hour)
 
             shift_num = 10
             df_temp0 = copy.deepcopy(df0)
             for kan1 in range(shift_num):
                 df0 = pd.concat(
-                    [df0, df_temp0.groupby('FlightRoute').shift(periods=kan1 + 1).add_suffix(f'_shifted{kan1 + 1}')],
+                    [df0, df_temp0.groupby('route').shift(periods=kan1 + 1).add_suffix(f'_shifted{kan1 + 1}')],
                     axis=1)
 
             df0.dropna(inplace=True)
 
-            filtered_columns_list = ['year', 'month', 'day', 'dayofweek', 'hour', 'FlightRoute', 'is_holiday']
-            all_org_rows_list = filtered_columns_list + ['PaxWeight']
+            filtered_columns_list = ['year', 'month', 'day', 'dayofweek', 'hour', 'route', 'is_holiday']
+            all_org_rows_list = filtered_columns_list + ['paxWeight']
             for kan2 in range(shift_num):
                 filtered_columns_list_temp = [x + f'_shifted{kan2 + 1}' for x in all_org_rows_list]
                 for x in filtered_columns_list_temp:
                     filtered_columns_list.append(x)
-            filtered_columns_list.append('PaxWeight')
+            filtered_columns_list.append('paxWeight')
 
             df1 = df0.filter(filtered_columns_list)
 
@@ -72,6 +83,7 @@ def load_pred_pretrained_model(df_dict):
             if not json.loads(api_result.text)['success']:
                 print(f'y_pred:{y_pred} with {pkFlightInformation} did not recorded in DB.')
         except:
+            token = api_token_handler()
             result_data = {
                 "fkFlightInformation": int(pkFlightInformation),
                 "load": 0,
