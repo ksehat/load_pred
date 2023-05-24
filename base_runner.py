75f77faa4cbbae6f9c829a74ae7b2e24eb657fc2
ksehat <kan.sehat@gmail.com>
2023-05-24 13:52:00 +0330

    .

--- base_runner.py
+++ base_runner.py
@@ -4,7 +4,7 @@ from functions import api_token_handler
 from datetime import datetime as dt
 
 while True:
-    if dt.now().hour == 10 and dt.now().minute == 38:
+    if dt.now().hour == 11 and dt.now().minute == 31:
         token = api_token_handler()
         r = requests.get(url='http://192.168.115.10:8083/api/FlightLoadEstimate/GetAllFutureFlights',
                          headers={'Authorization': f'Bearer {token}'})
