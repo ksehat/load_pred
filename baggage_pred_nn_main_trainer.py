8c3d67c85c4e284e5ad9d28e8025d4edba313aa5
ksehat <kan.sehat@gmail.com>
2023-06-20 09:56:27 +0330

    the last training code for baggage models.

--- baggage_pred_nn_main_trainer.py
+++ baggage_pred_nn_main_trainer.py
@@ -39,16 +39,11 @@ df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
 df0['day'] = np.array(pd.DatetimeIndex(df0['departure']).day)
 df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['departure']).dayofweek)
 df0['hour'] = np.array(pd.DatetimeIndex(df0['departure']).hour)
-df0['is_holiday'][(np.array(pd.DatetimeIndex(df0['departure']).day_name()) == 'Friday')] = 1
-df0['is_holiday'][(np.array(pd.DatetimeIndex(df0['departure']).day_name()) == 'Thursday')] = 1
+df0['quarter'] = np.array(pd.DatetimeIndex(df0['departure']).quarter)
 
 df0['departure'] = pd.to_datetime(df0['departure'])
 df0.sort_values(by='departure', inplace=True)
 df0.reset_index(drop=True, inplace=True)
-
-holidays = df0.loc[df0['is_holiday'] == 1, 'departure']
-df0['days_until_holiday'] = holidays.reindex(df0.index, method='bfill').dt.date - df0['departure'].dt.date
-df0['days_until_holiday'] = pd.to_timedelta(df0['days_until_holiday']).dt.days
 
 le_route = LabelEncoder()
 df0['route'] = le_route.fit_transform(df0['route'])
@@ -56,12 +51,16 @@ df0['route'] = le_route.fit_transform(df0['route'])
 with open('baggage_deployed_models/label_encoder_baggage.pkl', 'wb') as f:
     pickle.dump(le_route, f)
 
-df0.drop(['departure', 'paxWeight', 'payLoad'], inplace=True, axis=1)
+df0.drop(['departure', 'paxWeight'], inplace=True, axis=1)
+
+# add Gaussian noise to x column
+# noise = np.random.normal(0, 1, len(df0))  # change mean and std as needed
+# df0["paxWeight"] = df0["paxWeight"] + noise
 
 col = df0.pop('baggage')
 df0.insert(len(df0.columns), 'baggage', col)
 
-shift_num = 10
+shift_num = 15
 df_temp0 = copy.deepcopy(df0)
 for i in range(shift_num):
     df0 = pd.concat([df0, df_temp0.groupby('route').shift(periods=i + 1).add_suffix(f'_shifted{i + 1}')], axis=1)
@@ -75,16 +74,15 @@ df0.insert(len(df0.columns), 'baggage', col)
 # df1 = pf.fit_transform(df0.iloc[:,:-1])
 # df2 = np.concatenate((df1,df0.iloc[:,-1:].values), axis=1)
 df2 = copy.deepcopy(np.array(df0))
-x_train, x_test, y_train, y_test = train_test_split(df2[:,:-1], df2[:,-1], test_size=0.1, shuffle=False)
-
+x_train, x_test, y_train, y_test = train_test_split(df2[:, :-1], df2[:, -1], test_size=0.1, shuffle=False)
 # =====================================================================================================
 model1 = manual_model_dense(x_train)
-model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-              loss=tf.keras.losses.MeanAbsoluteError(), metrics='mae')
+model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+               loss=tf.keras.losses.MeanAbsoluteError(), metrics='mae')
 
 es1 = EarlyStopping(monitor='val_loss', mode='min', patience=50, restore_best_weights=True)
 history = model1.fit(x_train, y_train,
-                    validation_data=(x_test, y_test), callbacks=es1, epochs=10000, batch_size=50)
+                     validation_data=(x_test, y_test), callbacks=es1, epochs=10000, batch_size=50)
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -96,15 +94,15 @@ plt.show()
 
 y_pred_train1 = model1.predict(x_train)
 y_pred_test1 = model1.predict(x_test)
-x_train2 = x_train[(abs(y_pred_train1.reshape(-1)-y_train) >= 300)]
-y_train2 = y_train[(abs(y_pred_train1.reshape(-1)-y_train) >= 300)]
+x_train2 = x_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 300)]
+y_train2 = y_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 300)]
 # =====================================================================================================
-model2 = GradientBoostingRegressor()
+model2 = GradientBoostingRegressor(max_depth=5)
 model2.fit(x_train2, y_train2)
 y_pred_train2 = model2.predict(x_train)
 y_pred_test2 = model2.predict(x_test)
-x_train3 = np.concatenate((y_pred_train1.reshape(-1,1),y_pred_train2.reshape(-1,1)), axis=1)
-x_test3 = np.concatenate((y_pred_test1.reshape(-1,1),y_pred_test2.reshape(-1,1)), axis=1)
+x_train3 = np.concatenate((y_pred_train1.reshape(-1, 1), y_pred_train2.reshape(-1, 1)), axis=1)
+x_test3 = np.concatenate((y_pred_test1.reshape(-1, 1), y_pred_test2.reshape(-1, 1)), axis=1)
 # =====================================================================================================
 input_shape = x_train3.shape[1]
 input_layer = keras.Input(shape=input_shape)
@@ -117,12 +115,12 @@ x3 = layers.Dense(5, activation="relu")(x2)
 # output_layer1 = layers.Dense(3)(x5)
 output_layer = layers.Dense(1)(x3)
 model3 = keras.Model(inputs=input_layer, outputs=output_layer)
-model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-              loss=tf.keras.losses.MeanAbsoluteError(), metrics='mae')
+model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+               loss=tf.keras.losses.MeanAbsoluteError(), metrics='mae')
 
-es3 = EarlyStopping(monitor='val_loss', mode='min', patience=50, restore_best_weights=True)
+es3 = EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True)
 history = model3.fit(x_train3, y_train,
-                    validation_data=(x_test3, y_test), callbacks=es3, epochs=10000, batch_size=100)
+                     validation_data=(x_test3, y_test), callbacks=es3, epochs=10000, batch_size=100)
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -145,6 +143,8 @@ for i in range(0, 1000, 100):
               df_result['error']))
 print(np.mean(np.abs(df_result['error'])))
 model1.save('baggage_deployed_models/baggage_model1.h5')
+model1.save_weights('baggage_deployed_models/baggage_model1_weights.h5')
 filename = 'baggage_deployed_models/baggage_model2.sav'
 pickle.dump(model2, open(filename, 'wb'))
 model3.save('baggage_deployed_models/baggage_model3.h5')
+model3.save_weights('baggage_deployed_models/baggage_model3_weights.h5')
