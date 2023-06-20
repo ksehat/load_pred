a8ff2f91beef0f6d59d4c121b239e8fbd0331a2b
ksehat <kan.sehat@gmail.com>
2023-06-20 09:58:03 +0330

    1. the API data is changed and there is no need to extra processings. 2. the code is refactored to be compatible with the new data.

--- baggage_pred_pretrained_model.py
+++ baggage_pred_pretrained_model.py
@@ -11,7 +11,6 @@ from functions import api_token_handler
 import joblib
 
 
-
 def baggage_pred_pretrained_model():
     token = api_token_handler()
     df_past = pd.DataFrame(
@@ -28,6 +27,10 @@ def baggage_pred_pretrained_model():
                                 ).text)['getAllFutureFlightsBaggageResponseItemViewModels']).sort_values(by='departure')
 
     df_future['departure'] = pd.to_datetime(df_future['departure'])
+    df_past['departure'] = pd.to_datetime(df_past['departure'])
+
+    df_past.sort_values(by='departure', inplace=True)
+    df_past.reset_index(drop=True, inplace=True)
 
     with open('baggage_deployed_models\label_encoder_baggage.pkl', 'rb') as f:
         le = pickle.load(f)
@@ -41,9 +44,10 @@ def baggage_pred_pretrained_model():
             flightdate = df_future.iloc[i:i + 1, :]['departure'].values[0]
 
             df0 = pd.concat([df_past, df_future.iloc[i:i + 1, :]], axis=0, ignore_index=True)
-            df0.drop(['pkFlightInformation', 'paxWeight', 'payLoad'], axis=1, inplace=True)
+            df0.drop(['pkFlightInformation'], axis=1, inplace=True)
 
-            df0['route'] = df0['route'].apply(lambda x: le.transform([x])[0] if x in le.classes_ else len(le.classes_)+1)
+            df0['route'] = df0['route'].apply(
+                lambda x: le.transform([x])[0] if x in le.classes_ else len(le.classes_) + 1)
 
             df0['baggage'] = df0['baggage'].str.split('/', expand=True)[1]
             df0['baggage'] = df0['baggage'].str.split(' ', expand=True)[0]
@@ -54,58 +58,54 @@ def baggage_pred_pretrained_model():
             df0['day'] = np.array(pd.DatetimeIndex(df0['departure']).day)
             df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['departure']).dayofweek)
             df0['hour'] = np.array(pd.DatetimeIndex(df0['departure']).hour)
-            df0['is_holiday'][(np.array(pd.DatetimeIndex(df0['departure']).day_name()) == 'Friday')] = 1
-            df0['is_holiday'][(np.array(pd.DatetimeIndex(df0['departure']).day_name()) == 'Thursday')] = 1
-
-            df0['departure'] = pd.to_datetime(df0['departure'])
-            df0.sort_values(by='departure', inplace=True)
-            df0.reset_index(drop=True, inplace=True)
-
-            holidays = df0.loc[df0['is_holiday'] == 1, 'departure']
-            df0['days_until_holiday'] = holidays.reindex(df0.index, method='bfill').dt.date - df0['departure'].dt.date
-            df0['days_until_holiday'] = pd.to_timedelta(df0['days_until_holiday']).dt.days
+            df0['quarter'] = np.array(pd.DatetimeIndex(df0['departure']).quarter)
 
             df0['route'] = le.fit_transform(df0['route'])
-            df0.drop(['departure'], inplace=True, axis=1)
+            df0.drop(['departure', 'paxWeight'], inplace=True, axis=1)
 
-            shift_num = 10
+            col = df0.pop('baggage')
+            df0.insert(len(df0.columns), 'baggage', col)
+
+            shift_num = 15
             df_temp0 = copy.deepcopy(df0)
             for kan1 in range(shift_num):
                 df0 = pd.concat(
                     [df0, df_temp0.groupby('route').shift(periods=kan1 + 1).add_suffix(f'_shifted{kan1 + 1}')],
                     axis=1)
 
-            df0.dropna(inplace=True)
-
-            col = df0.pop('baggage')
-            df0.insert(len(df0.columns), 'baggage', col)
+            df0.drop('baggage', inplace=True, axis=1)
+            df1 = copy.deepcopy(np.array(df0))
 
             model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
+            model1.load_weights('baggage_deployed_models/baggage_model1_weights.h5')
             model2 = joblib.load('baggage_deployed_models/baggage_model2.sav')
             model3 = keras.models.load_model('baggage_deployed_models/baggage_model3.h5')
+            model3.load_weights('baggage_deployed_models/baggage_model3_weights.h5')
 
-            df1 = copy.deepcopy(np.array(df0))
-            x_result = df1[-1, :-1]
-            y_pred1 = model1.predict(x_result.reshape(1,-1))
-            y_pred2 = model2.predict(x_result.reshape(1,-1))
+            x_result = df1[-1].reshape(1, -1)
+            y_pred1 = model1.predict(x_result)
+            y_pred2 = model2.predict(x_result)
             x_result_final = np.concatenate((y_pred1.reshape(-1, 1), y_pred2.reshape(-1, 1)), axis=1)
-            y_pred_final = model3.predict(x_result_final.reshape(1,-1))
+            y_pred_final = model3.predict(x_result_final.reshape(1, -1))
 
-            token = api_token_handler()
-            result_data = {
-                "fkFlightInformation": int(pkFlightInformation),
-                "load": float(y_pred_final[0][0]),
-                "flightCount": 1,
-                "flightDate": str(flightdate).split('.')[0].replace('T',' ')
-            }
-            api_result = requests.post(url='http://192.168.115.10:8081/api/FlightBaggageEstimate/CreateFlightBaggageEstimate',
-                                       json=result_data,
-                                       headers={'Authorization': f'Bearer {token}',
-                                                'Content-type': 'application/json',
-                                                })
-            if not json.loads(api_result.text)['success']:
-                print(f'y_pred:{y_pred_final} with {pkFlightInformation} did not recorded in DB.')
+            if y_pred_final[0][0] >= 100:
+                token = api_token_handler()
+                result_data = {
+                    "fkFlightInformation": int(pkFlightInformation),
+                    "load": float(y_pred_final[0][0]),
+                    "flightCount": 1,
+                    "flightDate": str(flightdate).split('.')[0].replace('T', ' ')
+                }
+                api_result = requests.post(
+                    url='http://192.168.115.10:8081/api/FlightBaggageEstimate/CreateFlightBaggageEstimate',
+                    json=result_data,
+                    headers={'Authorization': f'Bearer {token}',
+                             'Content-type': 'application/json',
+                             })
+                if not json.loads(api_result.text)['success']:
+                    print(f'y_pred:{y_pred_final} with {pkFlightInformation} did not recorded in DB.')
         except:
+            print('An error occured.')
             token = api_token_handler()
             result_data = {
                 "fkFlightInformation": int(pkFlightInformation),
