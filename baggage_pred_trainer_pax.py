6edecf90f42ba2e562066e2219cb547e146f5ba1
ksehat <kan.sehat@gmail.com>
2023-07-05 13:53:24 +0330

    .

--- baggage_pred_trainer_pax.py
+++ baggage_pred_trainer_pax.py
@@ -17,6 +17,7 @@ from create_model import manual_model_dense
 from functions import api_token_handler
 from early_stopping_multiple import EarlyStoppingMultiple
 from save_training_weights import SaveWeights
+from pax_pred_pretrained_model import pax_pred_pretrained_model
 
 
 # physical_devices = tf.config.list_physical_devices("GPU")
@@ -25,7 +26,7 @@ from save_training_weights import SaveWeights
 
 def get_last_5(df, reverse_route, date):
     mask = (df['route'] == reverse_route) & (df['departure'] < date)
-    return df.loc[mask, 'paxWeight'].tail(5).tolist()
+    return df.loc[mask, 'baggage'].tail(5).tolist()
 
 
 def custom_label_encode(column):
@@ -44,7 +45,7 @@ def custom_label_encode(column):
             encoded_column.append(label)
             label += 1
     # Save the label_dict to a file
-    dump(label_dict, 'pax_deployed_models/label_dict.joblib')
+    dump(label_dict, 'baggage_pax_deployed_models/label_dict.joblib')
     return encoded_column
 
 
@@ -57,7 +58,11 @@ df0 = pd.DataFrame(
                                      }
                             ).text)['getAllPastFlightsBaggageResponseItemViewModels']).sort_values(by='departure')
 
-df0.drop(['pkFlightInformation', 'baggage'], axis=1, inplace=True)
+df0.drop(['pkFlightInformation'], axis=1, inplace=True)
+
+df0['baggage'] = df0['baggage'].str.split('/', expand=True)[1]
+df0['baggage'] = df0['baggage'].str.split(' ', expand=True)[0]
+df0['baggage'] = df0['baggage'].astype(float)
 
 df0['year'] = np.array(pd.DatetimeIndex(df0['departure']).year)
 df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
@@ -76,12 +81,14 @@ df0['route'] = custom_label_encode(df0['route'])
 # create a function to get the last 5 values of the baggage column for the reverse route
 last_5_values = df0.apply(lambda x: get_last_5(df0, -x['route'], x['departure']), axis=1)
 for kan4 in range(5):
-    df0[f'reverse_pax_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
+    df0[f'reverse_baggage_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
 
-df0.drop(['departure'], inplace=True, axis=1)
+df0.drop(['departure','paxWeight'], inplace=True, axis=1)
 
-col = df0.pop('paxWeight')
-df0.insert(len(df0.columns), 'paxWeight', col)
+df0['pax_pred'] = pax_pred_pretrained_model('pax_train')
+
+col = df0.pop('baggage')
+df0.insert(len(df0.columns), 'baggage', col)
 
 shift_num = 15
 df_temp0 = copy.deepcopy(df0)
@@ -90,52 +97,52 @@ for i in range(shift_num):
 
 df0.dropna(inplace=True)
 
-col = df0.pop('paxWeight')
-df0.insert(len(df0.columns), 'paxWeight', col)
+col = df0.pop('baggage')
+df0.insert(len(df0.columns), 'baggage', col)
 
 df2 = copy.deepcopy(np.array(df0))
-#
-# x_train = df2[:-2, :-1]
-# x_test = df2[-2:, :-1]
-# y_train = df2[:-2, -1]
-# y_test = df2[-2:, -1]
-#
-# np.savetxt('x_train.csv', x_train, delimiter=',')
-# np.savetxt('x_test.csv', x_test, delimiter=',')
-# np.savetxt('y_train.csv', y_train, delimiter=',')
-# np.savetxt('y_test.csv', y_test, delimiter=',')
+
+x_train = df2[:-2, :-1]
+x_test = df2[-2:, :-1]
+y_train = df2[:-2, -1]
+y_test = df2[-2:, -1]
+
+np.savetxt('x_train.csv', x_train, delimiter=',')
+np.savetxt('x_test.csv', x_test, delimiter=',')
+np.savetxt('y_train.csv', y_train, delimiter=',')
+np.savetxt('y_test.csv', y_test, delimiter=',')
 # =====================================================================================================
 x_train = np.loadtxt('x_train.csv', delimiter=',')
 x_test = np.loadtxt('x_test.csv', delimiter=',')
 y_train = np.loadtxt('y_train.csv', delimiter=',')
 y_test = np.loadtxt('y_test.csv', delimiter=',')
 # =====================================================================================================
-# model1 = manual_model_dense(x_train)
-# # model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
-# model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
-#                loss=tf.keras.losses.Huber(), metrics='mae')
-#
-# history = model1.fit(x_train, y_train,
-#                      validation_data=(x_test, y_test), callbacks=[
-#         SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights/model1/')], epochs=10000,
-#                      batch_size=50)
-# model1.save('pax_deployed_models/pax_model1.h5')
-#
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('Model loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-# plt.legend(['Train', 'Test'], loc='upper left')
-# plt.show()
+model1 = manual_model_dense(x_train)
+# model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
+model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6),
+               loss=tf.keras.losses.Huber(), metrics='mae')
 
-model1 = keras.models.load_model('pax_deployed_models/pax_model1.h5')
-model1.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights\model1/weights_epoch513.h5')
+history = model1.fit(x_train, y_train,
+                     validation_data=(x_test, y_test), callbacks=[
+        SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_pax_training_weights/model1/')], epochs=10000,
+                     batch_size=50)
+model1.save('baggage_pax_deployed_models/baggage_model1.h5')
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+model1 = keras.models.load_model('baggage_pax_deployed_models/baggage_model1.h5')
+model1.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_pax_training_weights\model1/weights_epoch1288.h5')
 
 y_pred_train1 = model1.predict(x_train)
 y_pred_test1 = model1.predict(x_test)
-x_train2 = x_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 2)]
-y_train2 = y_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 2)]
+x_train2 = x_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 800)]
+y_train2 = y_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 800)]
 # =====================================================================================================
 model2 = GradientBoostingRegressor(max_depth=5)
 model2.fit(x_train2, y_train2)
@@ -144,10 +151,10 @@ y_pred_test2 = model2.predict(x_test)
 x_train3 = np.concatenate((y_pred_train1.reshape(-1, 1), y_pred_train2.reshape(-1, 1)), axis=1)
 x_test3 = np.concatenate((y_pred_test1.reshape(-1, 1), y_pred_test2.reshape(-1, 1)), axis=1)
 
-filename = 'pax_deployed_models/pax_model2.sav'
+filename = 'baggage_pax_deployed_models/baggage_model2.sav'
 pickle.dump(model2, open(filename, 'wb'))
 
-model2 = joblib.load('pax_deployed_models/pax_model2.sav')
+model2 = joblib.load('baggage_pax_deployed_models/baggage_model2.sav')
 # =====================================================================================================
 input_shape = x_train3.shape[1]
 input_layer = keras.Input(shape=input_shape)
@@ -160,14 +167,14 @@ x3 = layers.Dense(5, activation="relu")(x2)
 # output_layer1 = layers.Dense(3)(x5)
 output_layer = layers.Dense(1)(x3)
 model3 = keras.Model(inputs=input_layer, outputs=output_layer)
-model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
                loss=tf.keras.losses.Huber(), metrics='mae')
 
 history = model3.fit(x_train3, y_train,
                      validation_data=(x_test3, y_test), callbacks=[
-        SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights/model3/')], epochs=10000,
+        SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_pax_training_weights/model3/')], epochs=10000,
                      batch_size=100)
-model3.save('pax_deployed_models/pax_model3.h5')
+model3.save('baggage_pax_deployed_models/baggage_model3.h5')
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -177,8 +184,8 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
-model3 = keras.models.load_model('pax_deployed_models/pax_model3.h5')
-model3.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights\model3/weights_epoch733.h5')
+model3 = keras.models.load_model('baggage_pax_deployed_models/baggage_model3.h5')
+model3.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_pax_training_weights\model3/weights_epoch2140.h5')
 # =====================================================================================================
 y_pred = model3.predict(x_test3).reshape(1, -1)
 y_actual = y_test.reshape(1, -1)
@@ -186,9 +193,9 @@ y_actual = y_test.reshape(1, -1)
 df_result = pd.DataFrame({'pred': y_pred.reshape(-1),
                           'actual': y_actual.reshape(-1)})
 df_result['error'] = df_result['actual'] - df_result['pred']
-for i in range(0, 10, 1):
-    print(f'Size of the errors between {i}kg to {i + 1}kg',
-          len(abs(df_result['error'])[((abs(df_result['error']) >= i) & (abs(df_result['error']) < i + 1 ))]) / len(
+for i in range(0, 1000, 100):
+    print(f'Size of the errors between {i}kg to {i + 100}kg',
+          len(abs(df_result['error'])[((abs(df_result['error']) >= i) & (abs(df_result['error']) < i + 100))]) / len(
               df_result['error']))
 print(np.mean(np.abs(df_result['error'])))
 
--- pax_pred_pretrained_model.py
+++ pax_pred_pretrained_model.py
@@ -1,19 +1,16 @@
-import time
 import pandas as pd
 import requests
 import json
 import copy
-import pickle
 import numpy as np
 import keras
 from functions import api_token_handler
 import joblib
-import schedule
 from joblib import load
 
 
 def apply_label_dict(column):
-    label_dict = load('baggage_deployed_models/label_dict.joblib')
+    label_dict = load('pax_deployed_models/label_dict.joblib')
     encoded_column = []
     for route in column:
         origin, destination = route.split('>')
@@ -30,10 +27,10 @@ def apply_label_dict(column):
 
 def get_last_5(df, reverse_route, date):
     mask = (df['route'] == reverse_route) & (df['departure'] < date)
-    return df.loc[mask, 'baggage'].tail(5).tolist()
+    return df.loc[mask, 'paxWeight'].tail(5).tolist()
 
 
-def baggage_pred_pretrained_model():
+def pax_pred_pretrained_model(who_is_calling=None):
     token = api_token_handler()
     df_past = pd.DataFrame(
         json.loads(requests.get(url='http://192.168.115.10:8081/api/FlightBaggageEstimate/GetAllPastFlightsBaggage',
@@ -57,20 +54,19 @@ def baggage_pred_pretrained_model():
     df_future['route'] = df_future['route'].apply(lambda x: x.replace("-", ">"))
     df_past['route'] = df_past['route'].apply(lambda x: x.replace("-", ">"))
 
+    if who_is_calling != 'pax_train':
+        df_future.drop('baggage', axis=1, inplace=True)
+        df_past.drop('baggage', axis=1, inplace=True)
+
     for i in range(len(df_future)):
         try:
             pkFlightInformation = df_future.iloc[i:i + 1, :]['pkFlightInformation'].values[0]
             flightdate = df_future.iloc[i:i + 1, :]['departure'].values[0]
-            sales_weight = df_future.iloc[i:i + 1, :]['salesWeight'].values[0]
 
             df0 = pd.concat([df_past, df_future.iloc[i:i + 1, :]], axis=0, ignore_index=True)
             df0.drop(['pkFlightInformation', 'salesWeight'], axis=1, inplace=True)
 
             df0['route'] = apply_label_dict(df0['route'])
-
-            df0['baggage'] = df0['baggage'].str.split('/', expand=True)[1]
-            df0['baggage'] = df0['baggage'].str.split(' ', expand=True)[0]
-            df0['baggage'] = df0['baggage'].astype(float)
 
             df0['year'] = np.array(pd.DatetimeIndex(df0['departure']).year)
             df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
@@ -82,12 +78,12 @@ def baggage_pred_pretrained_model():
             # create a function to get the last 5 values of the baggage column for the reverse route
             last_5_values = df0.apply(lambda x: get_last_5(df0, -x['route'], x['departure']), axis=1)
             for kan4 in range(5):
-                df0[f'reverse_baggage_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
+                df0[f'reverse_paxWeight_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
 
-            df0.drop(['departure', 'paxWeight'], inplace=True, axis=1)
+            df0.drop(['departure'], inplace=True, axis=1)
 
-            col = df0.pop('baggage')
-            df0.insert(len(df0.columns), 'baggage', col)
+            col = df0.pop('paxWeight')
+            df0.insert(len(df0.columns), 'paxWeight', col)
 
             shift_num = 15
             df_temp0 = copy.deepcopy(df0)
@@ -96,16 +92,25 @@ def baggage_pred_pretrained_model():
                     [df0, df_temp0.groupby('route').shift(periods=kan1 + 1).add_suffix(f'_shifted{kan1 + 1}')],
                     axis=1)
 
-            df0.drop('baggage', inplace=True, axis=1)
+            df0.drop('paxWeight', inplace=True, axis=1)
             df0.dropna(inplace=True)
             df1 = copy.deepcopy(np.array(df0))
 
-            model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
-            model1.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_training_weights\model1/weights_epoch1288.h5')
-            model2 = joblib.load('baggage_deployed_models/baggage_model2.sav')
-            model3 = keras.models.load_model('baggage_deployed_models/baggage_model3.h5')
-            model3.load_weights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage_training_weights\model3/weights_epoch2140.h5')
+            model1 = keras.models.load_model('pax_deployed_models/pax_model1.h5')
+            model1.load_weights(
+                'C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights\model1/weights_epoch513.h5')
+            model2 = joblib.load('pax_deployed_models/pax_model2.sav')
+            model3 = keras.models.load_model('pax_deployed_models/pax_model3.h5')
+            model3.load_weights(
+                'C:/Users\Administrator\Desktop\Projects\member_pred/pax_training_weights\model3/weights_epoch733.h5')
 
+            if who_is_calling == 'pax_train':
+                x_result_past = df1[:-1]
+                y_pred1_past = model1.predict(x_result_past)
+                y_pred2_past = model2.predict(x_result_past)
+                x_result_final_past = np.concatenate((y_pred1_past.reshape(-1, 1), y_pred2_past.reshape(-1, 1)), axis=1)
+                df0['pax_train'] = model3.predict(x_result_final_past)
+                return df0['pax_train']
 
             x_result = df1[-1].reshape(1, -1)
             y_pred1 = model1.predict(x_result)
@@ -113,7 +118,7 @@ def baggage_pred_pretrained_model():
             x_result_final = np.concatenate((y_pred1.reshape(-1, 1), y_pred2.reshape(-1, 1)), axis=1)
             y_pred_final = model3.predict(x_result_final.reshape(1, -1))
 
-            if y_pred_final[0][0] >= 100:
+            if y_pred_final[0][0] >= 40:
                 print(y_pred_final[0][0])
                 token = api_token_handler()
                 result_data = {
@@ -121,16 +126,17 @@ def baggage_pred_pretrained_model():
                     "load": float(y_pred_final[0][0]),
                     "flightCount": 1,
                     "flightDate": str(flightdate).split('.')[0].replace('T', ' '),
-                    "salesWeight": float(sales_weight)
                 }
                 api_result = requests.post(
-                    url='http://192.168.115.10:8081/api/FlightBaggageEstimate/CreateFlightBaggageEstimate',
+                    url='http://192.168.115.10:8081/api/FlightLoadEstimate/CreateFlightLoadEstimate',
                     json=result_data,
                     headers={'Authorization': f'Bearer {token}',
                              'Content-type': 'application/json',
                              })
                 if not json.loads(api_result.text)['success']:
                     print(f'y_pred:{y_pred_final} with {pkFlightInformation} did not recorded in DB.')
+            if who_is_calling == 'pax_train':
+                return y_pred_final[0][0]
         except:
             print('An error occured.')
             token = api_token_handler()
@@ -146,5 +152,3 @@ def baggage_pred_pretrained_model():
                                    'Content-type': 'application/json',
                                    })
     print('Prediction job is done.')
-
-
