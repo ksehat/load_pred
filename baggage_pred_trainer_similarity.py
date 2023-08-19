2c8c1e261892664fd1f648f09076a08cf5b28083
ksehat <kan.sehat@gmail.com>
2023-08-19 11:37:56 +0330

    model for new routes and routes with less samples is defined.

--- baggage_pred_trainer_similarity.py
+++ baggage_pred_trainer_similarity.py
@@ -47,111 +47,136 @@ def custom_label_encode(column):
             encoded_column.append(label)
             label += 1
     # Save the label_dict to a file
-    dump(label_dict, 'baggage_deployed_models/label_dict.joblib')
+    dump(label_dict, 'baggage/baggage_deployed_models/label_dict.joblib')
     return encoded_column
 
 
 # Load your data into a DataFrame
-# token = api_token_handler()
-# df0 = pd.DataFrame(
-#     json.loads(requests.get(url='http://192.168.115.10:8081/api/FlightBaggageEstimate/GetAllPastFlightsBaggage',
-#                             headers={'Authorization': f'Bearer {token}',
-#                                      'Content-type': 'application/json',
-#                                      }
-#                             ).text)['getAllPastFlightsBaggageResponseItemViewModels']).sort_values(by='departure')
-#
-# df0.drop(['pkFlightInformation'], axis=1, inplace=True)
-#
-# df0['baggage'] = df0['baggage'].str.split('/', expand=True)[1]
-# df0['baggage'] = df0['baggage'].str.split(' ', expand=True)[0]
-# df0['baggage'] = df0['baggage'].astype(float)
-#
-# df0['year'] = np.array(pd.DatetimeIndex(df0['departure']).year)
-# df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
-# df0['day'] = np.array(pd.DatetimeIndex(df0['departure']).day)
-# df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['departure']).dayofweek)
-# df0['hour'] = np.array(pd.DatetimeIndex(df0['departure']).hour)
-# df0['quarter'] = np.array(pd.DatetimeIndex(df0['departure']).quarter)
-#
-# df0['departure'] = pd.to_datetime(df0['departure'])
-# df0.sort_values(by='departure', inplace=True)
-# df0.reset_index(drop=True, inplace=True)
-#
-# # Apply the function to the route column and assign it to a new column
-# df0['route'] = custom_label_encode(df0['route'])
-#
-# # create a function to get the last 5 values of the baggage column for the reverse route
-# last_5_values = df0.apply(lambda x: get_last_5(df0, -x['route'], x['departure']), axis=1)
-# for kan4 in range(5):
-#     df0[f'reverse_baggage_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
-#
-# df0.drop(['departure', 'paxWeight'], inplace=True, axis=1)
-#
-# col = df0.pop('baggage')
-# df0.insert(len(df0.columns), 'baggage', col)
-#
-# shift_num = 15
-# df_temp0 = copy.deepcopy(df0)
-# for i in range(shift_num):
-#     df0 = pd.concat([df0, df_temp0.groupby('route').shift(periods=i + 1).add_suffix(f'_shifted{i + 1}')], axis=1)
-#
-# df0.dropna(inplace=True)
-#
-# col = df0.pop('baggage')
-# df0.insert(len(df0.columns), 'baggage', col)
-#
-# df1 = copy.deepcopy(df0)
-# df1.reset_index(drop=True, inplace=True)
-# n = 3  # number of similar rows to find
-# similarity_columns = list(df1.columns)[:-1]
-# df1_np = df1[similarity_columns].to_numpy()
-#
-# # # Find the n most similar rows for each row
-# # most_similar_rows = []
-# # for i in range(n + 1, len(df1)):
-# #     # Calculate the cosine similarity matrix for rows before the current row
-# #     if i >= 100:
-# #         similarity_matrix = cosine_similarity(df1[similarity_columns].iloc[i - 100:i + 1])
-# #     else:
-# #         similarity_matrix = cosine_similarity(df1[similarity_columns].iloc[:i + 1])
-# #
-# #     # Find the n most similar rows
-# #     row_similarity = similarity_matrix[-1]
-# #     most_similar_indices = row_similarity.argsort()[-n:][::-1]
-# #     most_similar_rows.append(df1.iloc[most_similar_indices,-1:].stack().to_frame().reset_index(drop=True).T)
-#
+token = api_token_handler()
+df0 = pd.DataFrame(
+    json.loads(requests.get(url='http://192.168.115.10:8081/api/FlightBaggageEstimate/GetAllPastFlightsBaggage',
+                            headers={'Authorization': f'Bearer {token}',
+                                     'Content-type': 'application/json',
+                                     }
+                            ).text)['getAllPastFlightsBaggageResponseItemViewModels']).sort_values(by='departure')
+
+df0.drop(['pkFlightInformation'], axis=1, inplace=True)
+
+df0['baggage'] = df0['baggage'].str.split('/', expand=True)[1]
+df0['baggage'] = df0['baggage'].str.split(' ', expand=True)[0]
+df0['baggage'] = df0['baggage'].astype(float)
+
+df0['year'] = np.array(pd.DatetimeIndex(df0['departure']).year)
+df0['month'] = np.array(pd.DatetimeIndex(df0['departure']).month)
+df0['day'] = np.array(pd.DatetimeIndex(df0['departure']).day)
+df0['dayofweek'] = np.array(pd.DatetimeIndex(df0['departure']).dayofweek)
+df0['hour'] = np.array(pd.DatetimeIndex(df0['departure']).hour)
+df0['quarter'] = np.array(pd.DatetimeIndex(df0['departure']).quarter)
+
+df0['departure'] = pd.to_datetime(df0['departure'])
+df0.sort_values(by='departure', inplace=True)
+df0.reset_index(drop=True, inplace=True)
+
+# Apply the function to the route column and assign it to a new column
+df0['route'] = custom_label_encode(df0['route'])
+
+# create a function to get the last 5 values of the baggage column for the reverse route
+last_5_values = df0.apply(lambda x: get_last_5(df0, -x['route'], x['departure']), axis=1)
+for kan4 in range(5):
+    df0[f'reverse_baggage_{kan4 + 1}'] = last_5_values.apply(lambda x: x[kan4] if len(x) > kan4 else None)
+
+df0.drop(['departure', 'paxWeight'], inplace=True, axis=1)
+
+col = df0.pop('baggage')
+df0.insert(len(df0.columns), 'baggage', col)
+
+shift_num = 15
+df_temp0 = copy.deepcopy(df0)
+for i in range(shift_num):
+    df0 = pd.concat([df0, df_temp0.groupby('route').shift(periods=i + 1).add_suffix(f'_shifted{i + 1}')], axis=1)
+
+col = df0.pop('baggage')
+df0.insert(len(df0.columns), 'baggage', col)
+
+# region new routes prediction which has many None values
+mask = df0.iloc[5000:].isnull().any(axis=1)
+# Use boolean indexing to filter the rows
+df0_for_new_routes = df0.iloc[5000:][mask]
+
+from sklearn.ensemble import HistGradientBoostingRegressor as hgbr
+model_for_new_routes = hgbr(max_depth=6, random_state=40)
+x_train_new_routes = df0_for_new_routes.iloc[:-100,:-1]
+x_test_new_routes = df0_for_new_routes.iloc[-100:,:-1]
+y_train_new_routes = df0_for_new_routes.iloc[:-100,-1:]
+y_test_new_routes = df0_for_new_routes.iloc[-100:,-1:]
+
+model_for_new_routes.fit(x_train_new_routes, y_train_new_routes)
+y_pred_new_routes = model_for_new_routes.predict(x_test_new_routes)
+
+from sklearn.metrics import mean_absolute_error as mae
+print(mae(y_test_new_routes,y_pred_new_routes))
+filename = 'baggage_new_routes/baggage_deployed_models/hgbr.pkl'
+pickle.dump(model_for_new_routes, open(filename, 'wb'))
+# endregion
+
+df0.dropna(inplace=True)
+
+df1 = copy.deepcopy(df0)
+df1.reset_index(drop=True, inplace=True)
+n = 3  # number of similar rows to find
+similarity_columns = list(df1.columns)[:-1]
+df1_np = df1[similarity_columns].to_numpy()
+
 # # Find the n most similar rows for each row
 # most_similar_rows = []
 # for i in range(n + 1, len(df1)):
-#     # Build a KDTree with the rows before the current row
+#     # Calculate the cosine similarity matrix for rows before the current row
 #     if i >= 100:
-#         tree = KDTree(df1_np[i - 100:i + 1])
+#         similarity_matrix = cosine_similarity(df1[similarity_columns].iloc[i - 100:i + 1])
 #     else:
-#         tree = KDTree(df1_np[:i + 1])
+#         similarity_matrix = cosine_similarity(df1[similarity_columns].iloc[:i + 1])
 #
 #     # Find the n most similar rows
-#     dist, ind = tree.query(df1_np[i:i + 1], k=n + 1)
-#     most_similar_indices = ind[0][:n+1] + i - 100 if i >=100 else ind[0][:n+1]
-#     most_similar_rows.append(df1.iloc[most_similar_indices].stack().to_frame().reset_index(drop=True).T)
+#     row_similarity = similarity_matrix[-1]
+#     most_similar_indices = row_similarity.argsort()[-n:][::-1]
+#     most_similar_rows.append(df1.iloc[most_similar_indices,-1:].stack().to_frame().reset_index(drop=True).T)
+
+# Find the n most similar rows for each row
+most_similar_rows = []
+for i in range(n + 1, len(df1)):
+    # Build a KDTree with the rows before the current row
+    if i >= 100:
+        tree = KDTree(df1_np[i - 100:i + 1])
+    else:
+        tree = KDTree(df1_np[:i + 1])
+
+    # Find the n most similar rows
+    dist, ind = tree.query(df1_np[i:i + 1], k=n + 1)
+    most_similar_indices = ind[0][:n+1] + i - 100 if i >=100 else ind[0][:n+1]
+    most_similar_rows.append(df1.iloc[most_similar_indices].stack().to_frame().reset_index(drop=True).T)
+
+# Create a new dataframe with the most similar rows as new columns
+arr1 = np.concatenate((np.array(most_similar_rows).squeeze(), df1.to_numpy()[n + 1:, -1].reshape(-1, 1)),
+    axis=1)
+arr3 = np.delete(arr1, 272, axis=1)
+
+ss = StandardScaler()
+arr1 = ss.fit_transform(arr3[:, :-1])
+
+filename = 'baggage/baggage_deployed_models/scaler.pkl'
+pickle.dump(ss, open(filename, 'wb'))
+
+arr2 = np.concatenate((arr1, arr3[:, -1].reshape(-1, 1)), axis=1)
+
+x_train = arr2[:-60, :-1]
+x_test = arr2[-60:, :-1]
+y_train = arr2[:-60, -1]
+y_test = arr2[-60:, -1]
 #
-# # Create a new dataframe with the most similar rows as new columns
-# arr1 = np.concatenate((np.array(most_similar_rows).squeeze(), df1.to_numpy()[n + 1:, -1].reshape(-1, 1)),
-#     axis=1)
-# arr3 = np.delete(arr1, 272, axis=1)
-#
-# ss = StandardScaler()
-# arr1 = ss.fit_transform(arr3[:, :-1])
-# arr2 = np.concatenate((arr1, arr3[:, -1].reshape(-1, 1)), axis=1)
-#
-# x_train = arr2[:22031, :-1]
-# x_test = arr2[22031:22066, :-1]
-# y_train = arr2[:22031, -1]
-# y_test = arr2[22031:22066, -1]
-#
-# np.savetxt('x_train_similarity.csv', x_train, delimiter=',')
-# np.savetxt('x_test_similarity.csv', x_test, delimiter=',')
-# np.savetxt('y_train_similarity.csv', y_train, delimiter=',')
-# np.savetxt('y_test_similarity.csv', y_test, delimiter=',')
+np.savetxt('x_train_similarity.csv', x_train, delimiter=',')
+np.savetxt('x_test_similarity.csv', x_test, delimiter=',')
+np.savetxt('y_train_similarity.csv', y_train, delimiter=',')
+np.savetxt('y_test_similarity.csv', y_test, delimiter=',')
 # =====================================================================================================
 x_train = np.loadtxt('x_train_similarity.csv', delimiter=',')
 x_test = np.loadtxt('x_test_similarity.csv', delimiter=',')
@@ -160,15 +185,15 @@ y_test = np.loadtxt('y_test_similarity.csv', delimiter=',')
 # =====================================================================================================
 # model1 = manual_model_dense(x_train)
 # # model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
-# model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+# model1.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=4e-5),
 #                loss=tf.keras.losses.Huber(), metrics='mae')
 #
 # history = model1.fit(x_train, y_train,
 #                      validation_data=(x_test, y_test), callbacks=[
-#         SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/test/baggage_similarity_training_weights/model1/')],
+#         SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage/baggage_similarity_training_weights/model1/')],
 #                      epochs=10000,
 #                      batch_size=50)
-# model1.save('baggage_deployed_models/baggage_model1.h5')
+# model1.save('baggage/baggage_deployed_models/baggage_model1.h5')
 #
 # plt.plot(history.history['loss'])
 # plt.plot(history.history['val_loss'])
@@ -178,47 +203,48 @@ y_test = np.loadtxt('y_test_similarity.csv', delimiter=',')
 # plt.legend(['Train', 'Test'], loc='upper left')
 # plt.show()
 
-model1 = keras.models.load_model('baggage_deployed_models/baggage_model1.h5')
+model1 = keras.models.load_model('baggage/baggage_deployed_models/baggage_model1.h5')
 model1.load_weights(
-    'C:/Users\Administrator\Desktop\Projects\member_pred/test/baggage_similarity_training_weights/model1/weights_epoch208.h5')
+    'C:/Users\Administrator\Desktop\Projects\member_pred/baggage/baggage_similarity_training_weights/model1/weights_epoch41.h5')
 
 y_pred_train1 = model1.predict(x_train)
 y_pred_test1 = model1.predict(x_test)
-x_train2 = x_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 800)]
-y_train2 = y_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 800)]
+x_train2 = x_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 400)]
+y_train2 = y_train[(abs(y_pred_train1.reshape(-1) - y_train) >= 400)]
 # =====================================================================================================
 model2 = GradientBoostingRegressor(max_depth=5)
 model2.fit(x_train2, y_train2)
+
+filename = 'baggage/baggage_deployed_models/baggage_model2.sav'
+pickle.dump(model2, open(filename, 'wb'))
+
+model2 = joblib.load('baggage/baggage_deployed_models/baggage_model2.sav')
+
 y_pred_train2 = model2.predict(x_train)
 y_pred_test2 = model2.predict(x_test)
 x_train3 = np.concatenate((y_pred_train1.reshape(-1, 1), y_pred_train2.reshape(-1, 1)), axis=1)
 x_test3 = np.concatenate((y_pred_test1.reshape(-1, 1), y_pred_test2.reshape(-1, 1)), axis=1)
-
-filename = 'baggage_deployed_models/baggage_model2.sav'
-pickle.dump(model2, open(filename, 'wb'))
-
-model2 = joblib.load('baggage_deployed_models/baggage_model2.sav')
 # =====================================================================================================
 input_shape = x_train3.shape[1]
 input_layer = keras.Input(shape=input_shape)
 x = input_layer
-x1 = layers.Dense(20, activation="relu")(x)
-x2 = layers.Dense(10, activation="relu")(x1)
+x1 = layers.Dense(50, activation="relu")(x)
+x2 = layers.Dense(20, activation="relu")(x1)
 x3 = layers.Dense(5, activation="relu")(x2)
 # x4 = layers.Dense(5, activation="relu")(x3)
 # x5 = layers.Dense(5, activation="relu")(x4)
 # output_layer1 = layers.Dense(3)(x5)
 output_layer = layers.Dense(1)(x3)
 model3 = keras.Model(inputs=input_layer, outputs=output_layer)
-model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+model3.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
                loss=tf.keras.losses.Huber(), metrics='mae')
 
 history = model3.fit(x_train3, y_train,
                      validation_data=(x_test3, y_test), callbacks=[
-        SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/test/baggage_similarity_training_weights/model3/')],
+        SaveWeights('C:/Users\Administrator\Desktop\Projects\member_pred/baggage/baggage_similarity_training_weights/model3/')],
                      epochs=10000,
                      batch_size=100)
-model3.save('baggage_deployed_models/baggage_model3.h5')
+model3.save('baggage/baggage_deployed_models/baggage_model3.h5')
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -228,9 +254,9 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
-model3 = keras.models.load_model('baggage_deployed_models/baggage_model3.h5')
+model3 = keras.models.load_model('baggage/baggage_deployed_models/baggage_model3.h5')
 model3.load_weights(
-    'C:/Users\Administrator\Desktop\Projects\member_pred/test/baggage_similarity_training_weights\model3/weights_epoch69.h5')
+    'C:/Users\Administrator\Desktop\Projects\member_pred/baggage/baggage_similarity_training_weights\model3/weights_epoch404.h5')
 # =====================================================================================================
 y_pred = model3.predict(x_test3).reshape(1, -1)
 y_actual = y_test.reshape(1, -1)
@@ -244,14 +270,14 @@ for i in range(0, 1000, 100):
               df_result['error']))
 print(np.mean(np.abs(df_result['error'])))
 
-from sklearn.metrics import mean_absolute_error as mae
-
-x_val = df2[:21944, :-1]
-y_true = df2[:21944, -1]
-y_pred1 = model1.predict(x_val)
-y_pred2 = model2.predict(x_val)
-x_val_final = np.concatenate((y_pred1.reshape(-1, 1), y_pred2.reshape(-1, 1)), axis=1)
-y_pred_final = model3.predict(x_val_final)
-
-error = mae(y_true, y_pred_final)
-print(error)
+# from sklearn.metrics import mean_absolute_error as mae
+#
+# x_val = df2[:21944, :-1]
+# y_true = df2[:21944, -1]
+# y_pred1 = model1.predict(x_val)
+# y_pred2 = model2.predict(x_val)
+# x_val_final = np.concatenate((y_pred1.reshape(-1, 1), y_pred2.reshape(-1, 1)), axis=1)
+# y_pred_final = model3.predict(x_val_final)
+#
+# error = mae(y_true, y_pred_final)
+# print(error)
