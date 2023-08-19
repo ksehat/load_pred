c635a51fa197b904a04f6144a410a53416c6014a
ksehat <kan.sehat@gmail.com>
2023-08-19 11:33:44 +0330

    .

--- bagage_pred_runner.py
+++ bagage_pred_runner.py
@@ -5,7 +5,7 @@ from baggage_pred_pretrained_model import baggage_pred_pretrained_model
 # use the schedule.every() method to specify the frequency and time of execution
 # for example, to run the hello function every 10 seconds
 baggage_pred_pretrained_model()
-schedule.every(1).hours.do(baggage_pred_pretrained_model)
+schedule.every(30).minutes.do(baggage_pred_pretrained_model)
 
 # use a while loop to keep the program running
 while True:
